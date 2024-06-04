864a13c461815be03fe3f15c491686343bae82cf
Sarun Olankranok <gopomelo@MacBook-Air.local>
2024-06-04 20:11:09 +0700

    update backend url

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // Base URL and credentials from environment variables
-const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+console.log("backendUrl :", backendUrl);
 const username = process.env.REACT_APP_BACKEND_USER_NAME || "";
 const password = process.env.REACT_APP_BACKEND_PASSWORD || "";
 
@@ -10,7 +11,7 @@ const encoded = btoa(`${username}:${password}`);
 
 // Create Axios instance with basic auth header
 const instance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: backendUrl,
   headers: {
     Authorization: `Basic ${encoded}`,
   },
